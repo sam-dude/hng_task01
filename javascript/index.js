31fd9a7ec6f8fc63e7c0a6417a8cea5a04659e3c
sam-dude <blendert5@gmail.com>
2023-09-10 14:08:38 +0100

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    //display day of the week
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var currentDate = new Date();
+    var currentDay = daysOfWeek[currentDate.getDay()];
+
+    var currentDayOfTheWeek = document.getElementById('day-of-the-week')
+    currentDayOfTheWeek.textContent =  currentDay;
+
+    //UTC
+    var currentUTCTime = currentDate.toISOString();
+    var UTCTimeElement = document.getElementById('UTC-time')
+    UTCTimeElement.textContent = currentUTCTime
+});
